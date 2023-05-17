c575ec9f0cc739d6de86b8be1d5b76223b6f80a2
lance-wilson <lance.wilson96@gmail.com>
2023-05-17 12:08:15 -0500

    Update test scripts and other files to pass tests.

---
+++ tests/scripts/my_layered_test_script.sh
@@ -0,0 +1,110 @@
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+#!/bin/bash
+
+# This test script produces the product defined in:
+# @package@/yaml_configs/product_inputs/layered.yaml
+# using the data_fusion processing workflow.
+
+# Any template values that must match between this test script and layered.yaml will be identified
+# in the template instructions in both files - ensure all required matching values are identical.
+
+# Note that all --fuse_* arguments are ordered - there can be any number included in the command line
+# call, and they will be appended in the order they are received. Ensure the --fuse_* arguments are
+# grouped together appropriately for each data type.
+
+# Identify and update all instances of @ found within this script appropriately.
+
+#   @package@ should be the name of this repository
+#   @my_layered_test_script@ indicates that the test output directory name should match
+#                            the test script name by convention
+
+# Fusion Final arguments: These are the arguments to specify the final output product, after layering
+#                         each individual "fuse" product specification below. The "fusion_final" arguments
+#                         indicate how to bring all the individual products together into a single "final" product.
+#   @My-Layered-Product@: This is the name of your final product.  This MUST match @My-Layered-Product@ specified
+#                         within product_inputs/layered.yaml
+#   --fusion_final_source_name layered: The source name "layered" must match both:
+#                         * the name of the file product_inputs/layered.yaml and
+#                         * the key at the top level of product_inputs/layered.yaml
+#                         This is an arbitrarily defined "source" name, but it must be consistent.
+#   --fusion_final_platform_name multi: This is an entirely arbitrarily defined platform name.
+#                         It is only used for filename and title generation
+#                         (sets "platform_name" on the final xarray object)
+#   --fusion_final_output_format layered_imagery: Do not change this argument - the "layered_imagery"
+#                         output_format in the data_fusion repository is the magic that plots the
+#                         separate products together.
+
+# Individual fuse arguments: Each set of arguments fully defines a product for a single layer of the final
+# image.  Ensure the fuse arguments are grouped together appropriately for each layer:
+#   @datatype_N@ should reference the Nth datatype you are interested in processing (and will reference test data
+#              repository you set up through https://github.com/NRLMMD-GEOIPS/template_test_data
+#   @datafiles_N@ is the appropriate path to the data files of @datatype_N@ within the test data repo.
+#   @fuse_reader_name_N@ is the appropriate reader associated with your @datatype_N@
+#   @fuse_product_name_N@ is the product you would like to create from @datafile_N@ 
+#   @fuse_output_format_N@ is the output format you would like to use for plotting the current layer.
+#              Note this is most likely going to be a "clean" output format (no gridlines, coastlines,
+#              titles, etc), since we want to simply layer the different images, and the annotations
+#              will be applied via the "fusion_final_output_format".
+#              Valid image formats for layering must include fig, main_ax, mapobj inputs
+#                (If passed, use the existing fig, main_ax, and mapobj rather than recreating)
+#              Valid image formats for layering must support not producing output if
+#                output_fnames is None (so we can avoid producing images for each layer individually)
+#              See geoips imagery_clean.py and imagery_windbarbs_clean.py for examples of valid output formats.
+#   @fuse_dataset_name_N@: arbitrary string that defines the name of the current dataset.
+#              Any instance of @fuse_dataset_name_N@ in the template script must match those in the
+#              template product/layered.yaml file
+#   --fuse_order: Integer specifying the order of the layers.
+#              Lower numbers are on the bottom, higher numbers on the top
+
+# Note you may update other arguments within this test call, or add additional arguments.
+
+# Also - the first time you run your test script, it will prompt you to update your test outputs - at that point
+# you can populate the "tests/outputs" directory, and your next run will result in a 0 return value.
+
+# Ensure each test script is called from the @package@/tests/test_all.sh script.
+
+# Please create a separate test script for each piece of functionality you would like to test.
+
+data_fusion_procflow \
+      --compare_path "$GEOIPS_PACKAGES_DIR/template_fusion_plugin/tests/outputs/my_layered_test_script" \
+      --filename_formatter geoips_fname \
+      --procflow data_fusion \
+      --trackfile_parser bdeck_parser \
+      --trackfiles $GEOIPS_PACKAGES_DIR/geoips/tests/sectors/tc_bdecks/bwp142022.dat \
+      --fusion_final_product_name Layered \
+      --fusion_final_source_name layered \
+      --fusion_final_platform_name multi \
+      --fusion_final_output_format layered_imagery \
+      --fuse_files $GEOIPS_TESTDATA_DIR/test_data_scat/bg_data/ahi_20220911_1330_tc2022wp14muifa/* \
+          --fuse_reader_name ahi_hsd \
+          --fuse_product_name Infrared-Gray \
+          --fuse_output_format imagery_clean \
+          --fuse_dataset_name ir \
+          --fuse_order 0 \
+      --fuse_files $GEOIPS_TESTDATA_DIR/test_data_scat/data/20220911_metopb_byu_uhr_tc2022wp14muifa/MUIFA_20220911_51797_B_A-product.nc \
+          --fuse_reader_name ascat_uhr_netcdf  \
+          --fuse_product_name windspeed \
+          --fuse_output_format imagery_clean \
+          --fuse_dataset_name windspeed \
+          --fuse_order 1 \
+      --fuse_files $GEOIPS_TESTDATA_DIR/test_data_scat/data/20220911_metopb_knmi_tc2022wp14muifa/ascat_20220911_132700_metopb_51797_eps_o_250_3202_ovw.l2.nc \
+          --fuse_reader_name scat_knmi_winds_netcdf \
+          --fuse_product_name Windbarbs-Gray \
+          --fuse_output_format imagery_windbarbs_clean \
+          --fuse_dataset_name windbarbs \
+          --fuse_order 2 \
+
+curr_retval=$?
+
+exit $((curr_retval))
--- tests/test_all.sh
+++ tests/test_all.sh
@@ -10,31 +10,63 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-# Please identify and update all instances of "@" found in this script appropriately.
-# You will need to generate one or more test scripts that test your complete functionality,
-# (these scripts provide example geoips calls and sample output, as well as providing a full integration test),
-# and call each script within the test_all.sh script.  Do not rename this script or test directory - automated
-# integrations tests look for the tests/test_all.sh script for complete testing.
+# @ Please identify and update all instances of "@" found in this script appropriately.
+# @ You will need to generate one or more test scripts that test your
+# @ complete functionality, @(these scripts provide example geoips calls and
+# @ sample output, as well as providing a full integration test),
+# @ and call each script within the test_all.sh script.
+
+# Do not rename this script or test directory - automated integration
+# tests look for the tests/test_all.sh script for complete testing.
 
 #!/bin/bash
 
-# This should contain test calls to cover ALL required functionality tests for the @package@ repo.
+# This should contain test calls to cover ALL required functionality tests
+# for the @package@ repo.
 
 # The $GEOIPS tests modules sourced within this script handle:
-   # setting up the appropriate associative arrays for tracking the overall return value,
-   # calling the test scripts appropriately, and 
+   # setting up the appropriate associative arrays for tracking the overall
+   #   return value,
+   # calling the test scripts appropriately, and
    # setting the final return value.
 
 # Note you must use the variable "call" in the for the loop
 
-. $GEOIPS/tests/utils/test_all_pre.sh @package@
+# @ Pass the name of your package to "test_all_pre.sh", ie
+
+if [[ ! -d $GEOIPS_PACKAGES_DIR/geoips ]]; then
+    echo "Must CLONE geoips repository into \$GEOIPS_PACKAGES_DIR location"
+    echo "to use test_all.sh testing utility."
+    echo ""
+    echo "export GEOIPS_PACKAGES_DIR=<path_to_geoips_cloned_packages>"
+    echo "git clone https://github.com/NRLMMD-GEOIPS/geoips $GEOIPS_PACKAGES_DIR/geoips"
+    echo ""
+    exit 1
+fi
+if [[ ! -f $GEOIPS_PACKAGES_DIR/geoips/tests/utils/test_all_pre.sh ]]; then
+    echo "geoips-based testing utility does not exist:"
+    echo "  $GEOIPS_PACKAGES_DIR/geoips/tests/utils/test_all_pre.sh"
+    echo ""
+    echo "Please ensure geoips repo is CLONED in GEOIPS_PACKAGES_DIR location"
+    echo "and up to date."
+    exit 2
+fi
+
+# . $GEOIPS/tests/utils/test_all_pre.sh @package@
+. $GEOIPS/tests/utils/test_all_pre.sh my_package
+
+# @ NOTE: Update "template_fusion_plugin" paths below to point to your package's
+# @ test scripts, ie
+# @   $GEOIPS_PACKAGES_DIR/template_fusion_plugin -> $GEOIPS_PACKAGES_DIR/@package@
 
 echo ""
 # "call" used in test_all_run.sh
 for call in \
 \
             "$GEOIPS_PACKAGES_DIR/geoips/tests/utils/check_code.sh all `dirname $0`/../" \
-            "$GEOIPS_PACKAGES_DIR/@package@/tests/scripts/@my_layered_test_script@.sh"
+            "test_interfaces" \
+            "pytest $GEOIPS_PACKAGES_DIR/geoips/tests/test_pytest" \
+            "$GEOIPS_PACKAGES_DIR/template_fusion_plugin/tests/scripts/my_layered_test_script.sh"
 do
     . $GEOIPS/tests/utils/test_all_run.sh
 done
